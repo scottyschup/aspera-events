839ae3c9b34438084aff907f55495f574f906e93
Scott Schupbach <sschupbach@asperasoft.com>
2016-06-03 18:06:24 -0700

    A few initial view/routes/README changes

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SignUp!
-## An event planning tool for Aspera, an IBM Company (C)
+## An event planning tool for Aspera, an IBM<sup>&reg;</sup> Company
+
+Details to come!
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'events/create'
+
+  get 'events/destroy'
+
+  get 'events/edit'
+
+  get 'events/index'
+
+  get 'events/new'
+
+  get 'events/show'
+
+  get 'events/update'
+
   root 'projects#index'
   resources :projects
   resources :game_type
