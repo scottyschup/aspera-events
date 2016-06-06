2fc2cf2272c073f328e72bd78ed3b8f877c5c1e8
Scott Schupbach <ssschupbach@gmail.com>
2016-06-06 12:19:33 -0700

    Create index, new views; clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,5 @@
 Rails.application.routes.draw do
-  get 'events/create'
-
-  get 'events/destroy'
-
-  get 'events/edit'
-
-  get 'events/index'
-
-  get 'events/new'
-
-  get 'events/show'
-
-  get 'events/update'
-
-  root 'projects#index'
-  resources :projects
-  resources :game_type
+  root to: 'events#index'
+  resources :events
+  resources :game_types
 end
