536cf9229bea1bf30aad4489702f8a229d937c4a
Scott Schupbach <ssschupbach@gmail.com>
2016-07-20 15:55:43 -0700

    Add set_event; use find_or_init_by in set_user; refactor create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,36 @@
 class UsersController < ApplicationController
-  before_action :set_user
+  before_action :set_user, :set_event
 
   def create
     if @user.update(user_params)
-      @event = Event.find(params[:user][:event_id])
       @user.events << @event unless @user.events.include?(@event)
-      @user.save
-      flash[:notice] = ["Successsss!"]
-      redirect_to action: :show, controller: :events, id: @event.id
+      if @user.save
+        flash[:notice] = ["Successsss!"]
+      else
+        flash[:error] = @user.errors.full_messages
+      end
     else
       flash[:error] = @user.errors.full_messages
-      render :new
     end
+    redirect_to action: :show, controller: :events, id: @event.id
   end
 
   def destroy
-    @user.events.delete(Event.find(params[:event_id]))
-    redirect_to action: :show, controller: :events, id: params[:event_id]
+    redirect_to action: :show, controller: :events, id: @event.id
+    @user.events.delete(@event)
   end
 
   private
+
+  def set_event
+    @event = Event.find(params[:user][:event_id])
+  end
 
   def set_user
     if params[:id]
       @user = User.find(params[:id])
     else
-      @user = User.where(email: user_params[:email]).first || User.new
+      @user = User.find_or_initialize_by(email: user_params[:email])
     end
   end
 
